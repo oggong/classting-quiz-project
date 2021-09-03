3486567aa956b06f1f131fcc656d921233a630b7
Minseok Oh <oggong93@naver.com>
2021-09-03 18:00:43 +0900

    [2021 0903 18:00] final commit

--- src/views/IntroPage/IntroPage.js
+++ src/views/IntroPage/IntroPage.js
@@ -2,6 +2,8 @@ import React from 'react';
 import { getQuizCollection } from '../../api';
 import { useHistory } from 'react-router-dom';
 
+import classting_logo from '../../assets/classting_logo.png';
+
 const IntroPage = () => {
 
     let history = useHistory();
@@ -17,7 +19,8 @@ const IntroPage = () => {
 
     return (
         <div className='flex flex-col justify-center items-center overflow-hidden'>
-            <h1 className="font-bold text-500 text-6xl mt-32">클래스팅 간단 퀴즈</h1>
+            <img src={classting_logo} alt="classting_img_log" className="mt-32" />
+            <h1 className="font-bold text-500 text-6xl mt-10">클래스팅 간단 퀴즈</h1>
             <h1 className="font-bold text-500 text-3xl mx-auto mt-32">간단한 퀴즈를 풀어보세요</h1>
             <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold mt-32 py-2 px-4 rounded" onClick={clickAndGetQuiz}>
                 퀴즈 시작
